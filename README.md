0a6eae8deb08ce087c51539140e064787883d6f9
Simoso68 <87707341+Simoso68@users.noreply.github.com>
2024-08-09 00:58:32 +0200

    Early functioning version

---
+++ README.md
@@ -0,0 +1,17 @@
+# Llama-Lit
+
+[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/Simoso68/llama-lit/llama-lit/)
+
+Llama-Lit is a streamlit page providing a front-end to interact with the Llama model. \
+This is still in development and needs some adjustment to offer a good user experience. \
+\
+The backend was made using the [ollama](https://github.com/ollama/ollama-python) library. \
+Please keep in mind, that this module uses the first module listed when executing the function ```ollama.list()``` from the library itself. \
+Meaning, it is not guaranteed to use a Llama model. \
+\
+The frontend was made with [streamlit](https://streamlit.io). \
+If one wants to host this application themselves, they would need to install all [requirements](https://github.com/Simoso68/llama-lit/blob/main/requirements.txt) and run the script via ```streamlit run app.py```.
+
+## Using it
+
+🚧 This part of the README is not completed yet 🚧
---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import ollama as ol
+
+st.set_page_config("Llama-Lit", "🦙")
+
+class Message():
+    def __init__(self, name, content) -> None:
+        self.name = name
+        self.content = content
+
+def new_chat_message(message: Message):
+    if message.name == "ai":
+        AVATAR = "https://ollama.com/public/apple-touch-icon.png"
+    else:
+        AVATAR = None
+
+    with st.chat_message(message.name, avatar=AVATAR):
+        st.write(message.content.replace("<", "&#60;").replace(">", "&#62;").replace("\n", "<br>"), unsafe_allow_html=True)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for msg in st.session_state.messages:
+    new_chat_message(msg)
+
+
+USER_INPUT = st.chat_input("Ask llama ...")
+
+if USER_INPUT:
+    new_chat_message(Message("user", USER_INPUT))
+    st.session_state.messages.append(Message("user", USER_INPUT))
+    AI_STREAM = ol.chat(model=ol.list()["models"][0]["name"], messages=[{"role":"user", "content":USER_INPUT}], stream=True)
+    st.session_state.response = ""
+    RESPONSE_BODY = st.empty()
+    with st.chat_message("ai", avatar="https://ollama.com/public/apple-touch-icon.png"):
+        for chunk in AI_STREAM:
+            st.session_state.response += chunk["message"]["content"]
+            RESPONSE_BODY.write(st.session_state.response)
+    st.session_state.messages.append(Message("ai", st.session_state.response))
+    st.rerun()
